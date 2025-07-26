81acb33ecfe739439d83f680d628b38f8b734d0e
Abenezer Kifle <abenezerkifle000@gmail.com>
2025-07-26 06:23:03 -0700

    import admin users

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -1,4 +1,4 @@
-import AdminUsersCrud from "@/features/users/AdminUsersCrud";
+import AdminUsersCrud from "@/features/admin-users/AdminUsersCrud";
 export default function UserPage() {
   return (
     <div className="p-6 text-center text-neutral-400">
